d422c815cbf77fa188b0996cd99ec9416a46d6ff
Andrei Xavier <andrei_xavier@outlook.com>
2024-12-06 17:51:58 -0300

    exemplos de operações LINQ

---
+++ dotnet/LINQAdvancedOperations/ExampleGroupBy/Program.cs
@@ -0,0 +1,47 @@
+﻿//agrupa elementos de uma coleção com base em uma chave
+
+example2();
+
+void example1(){
+
+    var employees = new[]
+    {
+        new { Name = "John", Department = "HR"},
+        new { Name = "Anna", Department = "IT"},
+        new { Name = "Mike", Department = "IT"},
+        new { Name = "Sara", Department = "HR"},
+    };
+
+    var grouped = employees.GroupBy(e => e.Department);
+
+    foreach (var group in grouped)
+    {
+        Console.WriteLine($"Department: {group.Key}");
+
+        foreach (var employee in group)
+        {
+            Console.WriteLine($" - {employee.Name}");
+        }
+    }
+}
+
+void example2(){
+
+    var transactions = new[]
+    {
+        new { Id = 1, Amount = 100, Category = "Food" },
+        new { Id = 2, Amount = 200, Category = "Transport" },
+        new { Id = 3, Amount = 150, Category = "Food" },
+        new { Id = 4, Amount = 300, Category = "Shopping" },
+        new { Id = 5, Amount = 100, Category = "Transport" },
+    };
+
+    var grouped = transactions.GroupBy(t => t.Category);
+
+    foreach(var group in grouped)
+    {
+        Console.WriteLine($"Category: {group.Key}");
+        Console.WriteLine($"Total: {group.Sum(t => t.Amount)}");
+        Console.WriteLine();
+    }
+}
---
+++ dotnet/LINQAdvancedOperations/ExampleJoin/Program.cs
@@ -0,0 +1,59 @@
+﻿//une duas coleções com base em uma chave em comum
+
+example2();
+
+void example1()
+{
+    var customers = new[]
+    {
+        new { Id = 1, Name = "John"},
+        new { Id = 2, Name = "Anna"}
+    };
+
+    var orders = new[]
+    {
+        new { CustomerId = 1, Product = "Laptop"},
+        new { CustomerId = 2, Product = "Phone"},
+    };
+
+    var result = customers.Join(orders,
+        customer => customer.Id,
+        order => order.CustomerId,
+        (customer, order) => new { customer.Name, order.Product });
+
+    foreach(var item in result)
+    {
+        Console.WriteLine($"{item.Name} comprou um {item.Product}");
+    }
+}
+
+void example2()
+{
+    var authors = new[]
+    {
+        new { Id = 1, Name = "Author A"},
+        new { Id = 2, Name = "Author B"},
+    };
+
+    var books = new[]
+    {
+        new { AuthorId = 1, Title = "Book 1" },
+        new { AuthorId = 1, Title = "Book 2" },
+        new { AuthorId = 2, Title = "Book 3" },
+    };
+
+    var result = authors.GroupJoin(books,
+        author => author.Id,
+        book => book.AuthorId,
+        (author, book) => new { author.Name, book });
+
+    foreach(var item in result)
+    {
+        Console.WriteLine($"Author: {item.Name}");
+
+        foreach(var book in item.book)
+        {
+            Console.WriteLine($" - {book.Title}");
+        }
+    }
+}
